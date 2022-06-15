73efb7a9dbf1b45cf6407197378380da70eeb8ac
SHUBHI <shubhiagarwal909@gmail.com>
2022-06-15 14:56:59 +0530

    gui implementation using java

---
+++ src/Example1.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Frame f=new Frame("My first frame");
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+}
